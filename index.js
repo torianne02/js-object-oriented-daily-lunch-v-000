d65bb40b4af2f0b62244f456e55a23cdb13a2238
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 19:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+const Neighborhood = (() => {
+  let neighborhoodIds = 1;
+  return class {
+    constructor(name) {
+      this.id = neighborhoodIds++;
+      this.name = name;
+
+      store.neighborhoods.push(this);
+    }
+
+    costumers() {
+      return store.customers.filter(customer => customer.neighborhoodId === this.id);
+    }
+
+    meals() {
+      const allMeals = this.costumers().map(customer => customer.meals());
+      const merged = [].concat.apply([], allMeals);
+      return [...new Set(merged)];
+    }
+
+    deliveries() {
+      return store.deliveries.filter(delivery => deliver.neighborhoodId === this.id);
+    }
+  };
+})();
